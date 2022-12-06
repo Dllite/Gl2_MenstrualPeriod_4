300d131fc5da92637ef2498db6f26acc17547bfd
Dllite <dilanzambou2@gmail.com>
2022-12-06 16:56:30 +0100

    Creation du menu

--- app/src/main/java/com/litetech/menstrualperiod/MainActivity.java
+++ app/src/main/java/com/litetech/menstrualperiod/MainActivity.java
@@ -2,13 +2,33 @@ package com.litetech.menstrualperiod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        skip = findViewById(R.id.skip);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome();
+                //openAddAccount();
+            }
+
+        });
+    }
+
+    public void openHome() {
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
     }
 }
